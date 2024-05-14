819d42c7b2bd5572daad16a19db787311e38ba7b
alessaless <zoccolaaless@gmail.com>
2024-05-14 16:33:35 +0200

    Modifica al progetto per consentire al sistema di non ricreare il db ad ogni avvio.

--- src/main/java/c15/dev/HeartCareApplication.java
+++ src/main/java/c15/dev/HeartCareApplication.java
@@ -1,14 +1,11 @@
 package c15.dev;
 
-import c15.dev.utils.DBPopulator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HeartCareApplication {
-	@Autowired
-	private DBPopulator populator;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HeartCareApplication.class, args);
